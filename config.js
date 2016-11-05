facbce5332934434e7e51240a7aced37336cdd65
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2016-11-05 17:10:45 -0400

    Add travis/elasticbeanstalk stuff

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = module.exports = {
+	port : 80,
+	church : {
+
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		server: process.env.DB_HOST, // You can use 'localhost\\instance' to connect to named instance
+		database: process.env.DB_NAME,
+		port: process.env.DB_PORT
+	}
+};
