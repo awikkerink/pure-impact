cda13f0d401f57b55354e45dcdc81b91051d53c5
Kevin Willms <Kevin.Willms@d2l.com>
2016-11-06 09:14:12 -0500

    renamed macro to summary and micro to church

---
+++ web/dist/js/pages/summary.js
@@ -0,0 +1,44 @@
+/*
+* Author: Abdullah A Almsaeed (Modified by Kevin Willms)
+* Date: 4 Jan 2014 (modified on Aug. 5, 2016)
+* Description:
+*      Data pull for main dashboard index.html
+*/
+
+$(function () {
+  "use strict";
+
+  $('#heading').text("Where Are The Churches in Kitchener?"); //Take most recent value
+
+  var graphCount = 0;
+  var graphTotal = 1;
+
+  //PopulateGraphMonthly(36, 30, 0, false, 2, '/api/churchAttendance?months=36', '', 'churchAttendance', '', 0, 'line', '', 'CB15', function(cb){ graphCount++ })
+
+  PopulatePieGraph('/api/religionBreakdown', 'religionBreakdown', '', 0, 'religion', 'value', 'doughnut', 'CB15', '')
+  PopulatePieGraph('/api/religionDenomationBreakdown', 'religionDenomationBreakdown', '', 0, 'denomination', 'value', 'pie', 'CB15', '')
+
+  var areGraphsDone = function(){
+    if (graphCount == graphTotal){
+      $(".chart-legend-item").click(function(){
+        updateDataset($(this))
+      });
+    }
+    else {
+      setTimeout(areGraphsDone , 250)
+    }
+  }
+
+  setTimeout(areGraphsDone , 250)
+
+  $.get('/api/churchProvince' + document.location.search, function (res) {
+    $('#numChurchProvince').text(numberWithCommas(res[0].count));
+  });
+  $.get('/api/churchCity' + document.location.search, function (res) {
+    $('#numChurchCity').text(numberWithCommas(res[0].count));
+  });
+  $.get('/api/religionBreakdown', function (res) {
+    $('#largestFaith').text(res[0].religion + " - " + numberWithCommas(res[0].value));
+    $('#smallestFaith').text(res[res.length-1].religion + " - " + numberWithCommas(res[res.length -1].value));
+  });
+});
