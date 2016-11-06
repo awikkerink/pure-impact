9bdd675634e2feb12a21b56aa49142fcac63921b
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2016-11-06 13:09:50 -0500

    Sort religion numbers and return 400 on bad request

--- server.js
+++ server.js
@@ -68,7 +68,11 @@ apiRoutes.use('/churchProvince',function (req, res, next) {
 
 apiRoutes.use('/churchCity',function (req, res, next) {
   var queryData = url.parse(req.url, true).query;
-  var City = queryData.City.toUpperCase()
+  if (!queryData.City) {
+    res.status(400, 'City query parameter required');
+    return;
+  }
+  var City = queryData.City.toUpperCase();
   var query = "SELECT COUNT(*) as count FROM Church where City = '" + City + "'"
   querydb(query, config.church, function(cbvalues) {
     res.status(200).json(cbvalues);
@@ -107,7 +111,7 @@ apiRoutes.use('/churchAttendanceAverage',function (req, res, next) { ///fake
 
 apiRoutes.use('/religionBreakdown',function (req, res, next) {
   var queryData = url.parse(req.url, true).query;
-  var query = 'SELECT religion, SUM(total) as value FROM "religionByArea" group by religion'
+  var query = 'SELECT religion, SUM(total) as value FROM "religionByArea" group by religion order by value desc'
   querydb(query, config.church, function(cbvalues) {
     res.status(200).json(cbvalues);
   });
